fa9c99e9855ee68ba74b9b5491359e5168a1e5b3
indus31 <indus31@hotmail.fr>
2025-07-19 16:31:38 +0200

    add a countdown for image title intro 1

--- client/src/app/anim-intro/title-intro/title-intro.component.ts
+++ client/src/app/anim-intro/title-intro/title-intro.component.ts
@@ -7,13 +7,29 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class TitleIntroComponent implements OnInit {
-  
+  private countdown: number = 3;
+  private intervalId: any;
 
   ngOnInit(): void {
    
-
+ this.startCountdown();
 
   }
 
+startCountdown() {
+  const imageElement: any = document.getElementById('imageTitle1');
+    this.intervalId = setInterval(() => {
+      this.countdown--;
+      console.log(`Temps restant : ${this.countdown} secondes`);
 
+      if (this.countdown <= 0) {
+        console.log('fin du premier titre !');
+        imageElement.style.display = 'none';
+        clearInterval(this.intervalId); // Arrête l'intervalle une fois le décompte terminé
+      }
+    }, 1000); // 1000 millisecondes = 1 seconde
+  }
 }
+
+
+
