79efbfc5755f699adde3747304c92af5a393b858
indus31 <indus31@hotmail.fr>
2025-07-19 17:30:17 +0200

    add imagetitle 2 to the intro

--- client/src/app/anim-intro/anim-intro.page.ts
+++ client/src/app/anim-intro/anim-intro.page.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, Renderer2 } from '@angular/core';
 import { NavController } from '@ionic/angular';
 
 @Component({
@@ -8,17 +8,18 @@ import { NavController } from '@ionic/angular';
   standalone: false,
 })
 export class AnimIntroPage implements OnInit {
+  showElements: boolean = true;
   timer: number = 10;
   private timerInterval: any;
   private isTimerRunning: boolean = false;
 
-  constructor(private navCtrl: NavController) { }
-
+  constructor(private navCtrl: NavController,private renderer: Renderer2) { }
+  
   ngOnInit() {
     this.startTimer();
   }
 
-  @HostListener('document:keypress', ['$event'])
+    @HostListener('document:keypress', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
     if (event.key === 'Enter') {
       this.navCtrl.navigateRoot('/home');
@@ -28,10 +29,13 @@ export class AnimIntroPage implements OnInit {
       } else {
         this.startTimer();
       }
+    } else if (event.key.toLowerCase() === 'm') {
+      this.showElements = !this.showElements; // Masquer ou afficher les éléments
     }
   }
 
   startTimer() {
+   const screenIntro = document.getElementById('screenIntro');
     if (!this.isTimerRunning) {
       this.isTimerRunning = true;
       this.timerInterval = setInterval(() => {
@@ -39,6 +43,8 @@ export class AnimIntroPage implements OnInit {
         if (this.timer <= 0) {
           this.stopTimer();
           this.navCtrl.navigateRoot('/home');
+        }else if(this.timer<= 7 ){
+         this.renderer.setStyle(screenIntro, 'backgroundColor', `#FFEFFF`);
         }
       }, 1000);
     }
@@ -51,4 +57,5 @@ export class AnimIntroPage implements OnInit {
       this.isTimerRunning = false;
     }
   }
+  
 }
--- client/src/app/anim-intro/title-intro/title-intro.component.ts
+++ client/src/app/anim-intro/title-intro/title-intro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-title-intro',
@@ -7,16 +7,17 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class TitleIntroComponent implements OnInit {
+  constructor(private renderer: Renderer2) {}
   private countdown: number = 3;
   private intervalId: any;
 
   ngOnInit(): void {
    
- this.startCountdown();
+ this.startTitleIntro();
 
   }
 
-startCountdown() {
+startTitleIntro() {
   const imageElement: any = document.getElementById('imageTitle1');
     this.intervalId = setInterval(() => {
       this.countdown--;
@@ -25,9 +26,22 @@ startCountdown() {
       if (this.countdown <= 0) {
         console.log('fin du premier titre !');
         imageElement.style.display = 'none';
+        this.showBlacksBand()
         clearInterval(this.intervalId); // Arrête l'intervalle une fois le décompte terminé
       }
     }, 1000); // 1000 millisecondes = 1 seconde
+  }
+  showBlacksBand() {
+    const bandeNoireSup = document.getElementById('bandeNoireSup');
+    const bandeNoireInf = document.getElementById('bandeNoireInf');
+
+    if (bandeNoireSup) {
+      this.renderer.setStyle(bandeNoireSup, 'display', 'block');
+    }
+
+    if (bandeNoireInf) {
+      this.renderer.setStyle(bandeNoireInf, 'display', 'block');
+    }
   }
 }
 
